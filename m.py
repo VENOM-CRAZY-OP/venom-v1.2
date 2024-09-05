ba01c33a0f9e420e3e1e1f43be8ee2b6eeb5ad1d
VENOM-CRAZY-OP <cyberecurityind@gmail.com>
2024-09-05 13:42:07 +0530

    Update m.py

--- m.py
+++ m.py
@@ -8,10 +8,10 @@ import os
 from keep_alive import keep_alive
 keep_alive()
 # insert your Telegram bot token here
-bot = telebot.TeleBot('TOKEN')
+bot = telebot.TeleBot('7188949782:AAHeV86SpD5rvn65Xrn9OCWU6c4QxCB7WPw')
 
 # Admin user IDs
-admin_id = ["your id"]
+admin_id = ["5588464519"]
 
 # File to store allowed user IDs
 USER_FILE = "users.txt"
